f680afe05688674c0d423dd824517bc06d9d9ea9
Farhad Khurami <fkhurami98@gmail.com>
2023-11-09 18:16:00 +0000

    Updated formatting and print statements

--- main.py
+++ main.py
@@ -9,14 +9,23 @@ import requests
 import urllib.parse
 from fake_useragent import UserAgent
 
-search_keywords: list= ["Lego 75280", "Lego 75309", "Lego 75342"]
+search_keywords: list = [
+    "Lego 75280",
+    "Lego 75309",
+    "Lego 75342",
+    "Lego 10497",
+    "Lego 76193",
+    "Lego 75324",
+    "Lego 75339",
+    "Lego 75331"
+]
 max_pages: int = 1
-items_per_page: int = 120       # 60, 120, 240
+items_per_page: int = 120  # 60, 120, 240
 
 
 def get_number_of_search_results(soup):
     # Find the correct <script> tag - adjust the criteria as needed for your specific case
-    script_tag = soup.find("script", text=re.compile(r'"text":"\d+ results"'))
+    script_tag = soup.find("script", string=re.compile(r'"text":"\d+ results"'))
     if script_tag:
         script_content = script_tag.string  # Get the string content of the <script> tag
         # This pattern looks for the number of results in the format '"text":"<number> results"'
@@ -97,31 +106,36 @@ def extract_data(soup):
 
 def save_to_csv(data, filename, include_headers=True):
     file_exists = os.path.isfile(filename)
-    
-    with open(filename, 'a', newline='', encoding='utf-8') as csvfile:
+
+    with open(filename, "a", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
         if include_headers and not file_exists:
-            writer.writerow([
-                "Title",
-                "Price",
-                "URL",
-                "Seller Info",
-                "Shipping Detail",
-                "Listing Type",
-                "Item Condition",
-                "Sold Date",
-            ])
+            writer.writerow(
+                [
+                    "Title",
+                    "Price",
+                    "URL",
+                    "Seller Info",
+                    "Shipping Detail",
+                    "Listing Type",
+                    "Item Condition",
+                    "Sold Date",
+                ]
+            )
         for item in data:
-            writer.writerow([
-                item["title"],
-                item["price"],
-                item["url"],
-                item["seller_info"],
-                item["shipping_detail"],
-                item["listing_type"],
-                item["item_condition"],
-                item["sold_date"],
-            ])
+            writer.writerow(
+                [
+                    item["title"],
+                    item["price"],
+                    item["url"],
+                    item["seller_info"],
+                    item["shipping_detail"],
+                    item["listing_type"],
+                    item["item_condition"],
+                    item["sold_date"],
+                ]
+            )
+
 
 if __name__ == "__main__":
     user_agent = UserAgent()
@@ -139,15 +153,15 @@ if __name__ == "__main__":
         soup = BeautifulSoup(response.content, "html.parser")
 
         number_of_searches = get_number_of_search_results(soup)
-        print(f"Total number of searches for '{keyword}':", number_of_searches)
+        print(f"[INFO] Total number of searches for '{keyword}':", number_of_searches)
 
         if number_of_searches is not None:
             max_pages = number_of_searches // items_per_page
             max_pages = max(1, max_pages)  # Ensure at least one page
-            print(f"Total pages to scrape for '{keyword}':", max_pages)
+            print(f"[INFO] Total pages to scrape for '{keyword}':", max_pages)
 
             for page_number in range(1, max_pages + 1):
-                print(f"Scraping page {page_number} for {keyword}...")
+                print(f"[WORKING] Scraping page {page_number} for {keyword}...")
 
                 # Generate the URL for the current page
                 base_url = f"https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw={encoded_keyword}&_sacat=0&LH_Sold=1&LH_Complete=1&LH_ItemCondition=1000&rt=nc&_pgn={page_number}&_ipg={items_per_page}"
@@ -162,9 +176,13 @@ if __name__ == "__main__":
                     save_to_csv(listings, filename, include_headers=True)
                 else:
                     save_to_csv(listings, filename, include_headers=False)
-                
-                print(f"Data from page {page_number} for {keyword} saved to '{filename}'.")
 
+                print(
+                    f"[SUCCESS] Data from page {page_number} for {keyword} saved to '{filename}'."
+                )
                 time.sleep(random.uniform(1, 10))
+            print()
         else:
-            print(f"Couldn't determine the number of searches for '{keyword}'. Skipping...")            
+            print(
+                f"[FINISHED] Couldn't determine the number of searches for '{keyword}'. Skipping..."
+            )
